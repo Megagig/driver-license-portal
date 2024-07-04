13ac1e81aa634480f412b95d9c5a4e17c5007917
ayopelumi <ayopelumi2014@gmail.com>
2024-07-04 15:50:30 -0700

    started the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
 import './index.css';
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
+    <>
     <div>
       <h1 className="text-3xl font-bold underline bg-orange-600">
         Nigeria Driver License Portal
       </h1>
       <p>Seamless Licensure For The Nigerian populace</p>
     </div>
+    <HomePage />
+    </>
   );
 }
 
---
+++ src/pages/home/components/Hero.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import img from "../../../assets/images/img2.jpg";
+import img2 from "../../../assets/images/img5.jpg";
+import img1 from "../../../assets/images/img6.jpg";
+import img4 from "../../../assets/images/img8.jpg";
+import img5 from "../../../assets/images/img9.jpg";
+
+export const Hero = () => {
+
+  let imgArray =[img, img1, img2,img4,img5]
+  const [index, setIndex] = useState(0)
+  useEffect(() => {
+    let interval=  setInterval(() => {
+      index ==2 ? setIndex(0) : setIndex(index+1);
+    }, 10000)
+    return () => {
+      clearInterval(interval)
+    };
+  }, [index]);
+console.log({index})
+  return (
+    <section className="flex flex-1 items-center bg-white flex-row h-screen">
+      <aside className='flex bg-[url("src/assets/images/img7.jpg")] p-24 justify-center items-center flex-1 flex-col'>
+        <span className="text-black w-3/5 text-[24px]/[38.4px] w-3/4 font-normal text-wrap text-left">
+          <h3 className="text-[32px]/[51.1px] font-semibold text-pretty mb-4">
+              SINC Partners is a service incubation company 
+          </h3>
+          Connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%).
+          SINC With Us
+        </span>
+        <button className=" mt-4 bg-[#22C55E] text-[18px]/[19.2px] text-white px-8 py-6 mr-auto font-medium rounded-full w-fit">Apply to SIP 1.0</button>
+
+      </aside>
+      <aside className="flex-1 h-full">
+          <img src={imgArray[index]} className='object-cover w-full h-full' alt="image" />
+      </aside>
+
+    </section>
+  );
+};
