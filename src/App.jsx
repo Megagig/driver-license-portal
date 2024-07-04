99316659e3bb9593f6c9dc466a21f682493a16c3
Megagig <megagigsoftwaresolutions.com>
2024-07-04 11:42:29 +0100

    Install Tailwind to the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div>
-      <h1>Nigeria Driver License Portal</h1>
+      <h1 className="text-3xl font-bold underline bg-orange-600">
+        Nigeria Driver License Portal
+      </h1>
       <p>Seamless Licensure For The Nigerian populace</p>
     </div>
   );
