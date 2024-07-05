41e15277388ed3bc8459e052621f4e4f089c03c8
IYKE <ikegwimik@gmail.com>
2024-07-05 17:55:26 +0100

    resolved all merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
+
 import './index.css';
 import Router from './router/Router';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Router />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+
+            <Router />
+
+        </div>
+    );
 }
 
 export default App;
