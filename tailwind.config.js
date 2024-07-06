de878c6fa55b97531e162a91e11ee1b05f02ca38
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-07-06 16:54:59 +0100

    config: Set custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
-    },
+      },
+      colors: {
+        'green': '#15803D',
+        'gray': '#3d4249',
+      }
     },
   },
   plugins: [],
