4a2bd8560f8e286444c33f49ef1bbf69f3e572f9
Megagig <megagigsoftwaresolutions.com>
2024-07-08 18:10:21 +0100

    Add the Signup page

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import AdminLogin from '../../views/auth/AdminLogin';
-import Login from '../../views/auth/Login';
-import Register from '../../views/auth/Register';
+import LoginPage from '../auth/login/LoginPage';
+import SignupPage from '../auth/signup/SignupPage';
 
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage Component Here...</h1>
+      <LoginPage />
+      <SignupPage />
     </div>
   );
 };
