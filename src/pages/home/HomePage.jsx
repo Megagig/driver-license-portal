c40ed36dbc80c8843682b51a4b39ee6771281138
Megagig <megagigsoftwaresolutions.com>
2024-07-08 13:12:33 +0100

    Remve Reissue form from homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReIssueLicensePage from '../reissue/ReIssueLicensePage';
 
 const HomePage = () => {
   return (
     <div>
       <h1>HomePage Component Here...</h1>
-      <ReIssueLicensePage />
     </div>
   );
 };
