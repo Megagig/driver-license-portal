89bb06457102082d5332b7191a265b9aaf33483c
Megagig <megagigsoftwaresolutions.com>
2024-07-08 17:09:34 +0100

    Created the Auth Boilerplate

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import AdminLogin from '../../views/auth/AdminLogin';
+import Login from '../../views/auth/Login';
+import Register from '../../views/auth/Register';
 
 const HomePage = () => {
   return (
