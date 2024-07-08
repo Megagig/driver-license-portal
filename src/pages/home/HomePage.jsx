01e62d506cf93523af4ee755298322701e26d391
ayopelumi <ayopelumi2014@gmail.com>
2024-07-08 08:11:21 -0700

    worked on license prcess component

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Hero } from "./components/Hero"
 import Services from './components/Services'
+import LicenseProcess from './components/LicenseProcess'
+
 const HomePage = () => {
     return (
         <div className='w-screen'>
             <Hero />
             <Services />
+            <LicenseProcess />
         </div>
     )
 }
