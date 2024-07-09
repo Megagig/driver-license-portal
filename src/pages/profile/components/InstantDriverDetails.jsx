4473218f6d835a8ad3d507280f3a97b28f9b6a3b
ayopelumi <ayopelumi2014@gmail.com>
2024-07-09 03:17:59 -0700

    worked on the layout

---
+++ src/pages/profile/components/InstantDriverDetails.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import SingleDetail from '../utils/SingleDetail';
+const InstantDriverDetails = () => {
+    return (
+         <div className='flex mt-6 p-6 relative border-4 border-custom-grey'>
+            <h4 className="p-3 bg-white text-3xl font-medium absolute top-[-29px]">Instant Driver Details</h4>
+            <div className='flex flex-col'>
+                <SingleDetail
+                    label='Joined'
+                    details='Ogundeji Oluwapelumi Ayodeji' />
+
+                <SingleDetail
+                    label='Last Renewal'
+                    details='Ogundeji Oluwapelumi Ayodeji'
+                />
+                <SingleDetail
+                    label='Next Renewal'
+                    details='Ogundeji Oluwapelumi Ayodeji'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default InstantDriverDetails
--- src/pages/profile/components/LoginDetails.jsx
+++ src/pages/profile/components/LoginDetails.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { FaEdit } from "react-icons/fa";
+import SingleDetail from '../utils/SingleDetail';
 
 const LoginDetails = () => {
     return (
         <div className='flex mt-6 p-6 relative border-4 border-custom-grey '>
-        <h4 className=" bg-white text-lg font-medium absolute top-[-16px]">Login Details</h4>
+        <h4 className="p-3 bg-white text-3xl font-medium absolute top-[-29px]">Login Details</h4>
+        <FaEdit className="bg-white text-3xl font-medium absolute top-[-16px] right-0 cursor-pointer"  />
         <div className='flex flex-col'>
-            <div>
-                <span>
-                    Password
-                </span>
-                <p>
-                    Ogundeji Oluwapelumi Ayodeji
-                </p>
-                <FaEdit />
-            </div>
+            <SingleDetail
+                label='Password'
+                details='Ogundeji Oluwapelumi Ayodeji' />
         </div>
     </div>
     )
