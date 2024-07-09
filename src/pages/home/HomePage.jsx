82c3f2a7d9edace75d207cdfbe7b5dd95c7cb64b
ayopelumi <ayopelumi2014@gmail.com>
2024-07-09 02:20:41 -0700

    started the user profile

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Profile from "../profile/Profile"
 import { Hero } from "./components/Hero"
 import LicenseProcess from './components/LicenseProcess'
 import Services from './components/Services'
@@ -9,6 +10,7 @@ const HomePage = () => {
             <Hero />
             <Services />
             <LicenseProcess />
+            <Profile />
         </div>
     )
 }
