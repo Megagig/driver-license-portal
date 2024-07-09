472f65ce51e450ad97a9e301c142b7651a1903d7
ayopelumi <ayopelumi2014@gmail.com>
2024-07-09 03:17:59 -0700

    worked on the layout

--- src/pages/profile/components/BasicDetails.jsx
+++ src/pages/profile/components/BasicDetails.jsx
@@ -1,47 +1,25 @@
 import React from 'react';
 import { FaEdit } from "react-icons/fa";
+import SingleDetail from '../utils/SingleDetail';
 
 const BasicDetails = () => {
     return (
-        <div className='flex mt-16 p-6 relative border-4 border-custom-grey'>
-        <h4 className=" bg-white text-2xl font-medium absolute top-[-16px]">Basic Details</h4>
-        <div className='flex flex-col'>
-            <div className="flex p-6 items-center gap-6">
-                <span className="flex-1 text-lg font-normal">
-                    Full Name
-                </span>
-                <p className="text-lg font-normal border-4 border-custom-grey p-4 flex-auto mr-6">
-                    Ogundeji Oluwapelumi Ayodeji
-                </p>
-                <FaEdit className="text-2xl" />
-            </div>
-            <div className="flex p-6 items-center gap-6">
-                <span className="flex-1 text-lg font-normal">
-                    Email
-                </span>
-                <p className="text-lg font-normal border-4 border-custom-grey p-4 flex-auto mr-6">
-                    Ogundeji Oluwapelumi Ayodeji
-                </p>
-                <FaEdit className="text-2xl"  />
-            </div>
-            <div className="flex p-6 items-center gap-6">
-                <span className=" flex-1 text-lg font-normal">
-                    Phone Number
-                </span>
-                <p className="text-lg font-normal border-4 border-custom-grey p-4 flex-auto mr-6">
-                    Ogundeji Oluwapelumi Ayodeji
-                </p>
-                <FaEdit className="text-2xl" />
-            </div>
-            <div className="flex p-6 items-center gap-6">
-                <span className="flex-1 text-lg font-normal">
-                    Date of Birth
-                </span>
-                <p className="text-lg font-normal border-4 border-custom-grey p-4 flex-auto mr-6">
-                    Ogundeji Oluwapelumi Ayodeji
-                </p>
-                <FaEdit className="text-2xl" />
-            </div>
+        <div className='flex mt-16 p-6 relative border-4 border-custom-grey w-full'>
+        <h4 className="p-3 bg-white text-2xl font-medium absolute top-[-29px]">Basic Details</h4>
+        <FaEdit className="bg-white text-3xl font-medium absolute top-[-16px] right-0 cursor-pointer"  />
+        < div className='flex flex-col w-full'>
+            <SingleDetail 
+                label="Full Name"
+                details ="Ogundeji Oluwapelumi Ayodeji" />
+            <SingleDetail
+                label='Email'
+                details="Ogundeji Oluwapelumi Ayodeji" />
+            <SingleDetail
+                label="Phone Number"
+                details='Ogundeji Oluwapelumi Ayodeji' />
+            <SingleDetail 
+                label='Date of Birth'
+                details='Ogundeji Oluwapelumi Ayodeji' />
         </div>
     </div>
     
