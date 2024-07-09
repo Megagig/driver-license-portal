40b8ca8512eb6ee6f2bd059e1020115b2d958e23
ayopelumi <ayopelumi2014@gmail.com>
2024-07-09 07:36:35 -0700

    added modals, form elements in the profile

--- src/pages/profile/components/BasicDetails.jsx
+++ src/pages/profile/components/BasicDetails.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import { FaEdit } from "react-icons/fa";
 import SingleDetail from '../utils/SingleDetail';
+import LoginDetailsForm from './Modals/LoginDetailsForm';
 
 const BasicDetails = () => {
+    const [showModal, setShowModal] = useState(false)
+
+    const handleEditBasicDetails = () => { 
+        setShowModal(true)
+     };
+
     return (
+        <>
         <div className='flex mt-16 p-6 relative border-4 border-custom-grey w-full'>
         <h4 className="p-3 bg-white text-2xl font-medium absolute top-[-29px]">Basic Details</h4>
-        <FaEdit className="bg-white text-3xl font-medium absolute top-[-16px] right-0 cursor-pointer"  />
-        < div className='flex flex-col w-full'>
+        <FaEdit className="bg-white text-3xl font-medium absolute top-[-16px] right-0 cursor-pointer" onClick={handleEditBasicDetails} />
+        < div className='flex flex-col'>
             <SingleDetail 
                 label="Full Name"
                 details ="Ogundeji Oluwapelumi Ayodeji" />
@@ -21,7 +29,10 @@ const BasicDetails = () => {
                 label='Date of Birth'
                 details='Ogundeji Oluwapelumi Ayodeji' />
         </div>
-    </div>
+        </div>
+        {showModal?
+        <LoginDetailsForm /> : null   }
+    </>
     
     )
 }
--- src/pages/profile/components/LoginDetails.jsx
+++ src/pages/profile/components/LoginDetails.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
 import { FaEdit } from "react-icons/fa";
 import SingleDetail from '../utils/SingleDetail';
 
 const LoginDetails = () => {
+    const [showModal, setShowModal] = useState(false)
+    const handleEditLoginDetails = () =>{
+        setShowModal(true)
+    }
+
     return (
         <div className='flex mt-6 p-6 relative border-4 border-custom-grey '>
         <h4 className="p-3 bg-white text-3xl font-medium absolute top-[-29px]">Login Details</h4>
-        <FaEdit className="bg-white text-3xl font-medium absolute top-[-16px] right-0 cursor-pointer"  />
+        <FaEdit className="bg-white text-3xl font-medium absolute top-[-16px] right-0 cursor-pointer" onClick={handleEditLoginDetails} />
         <div className='flex flex-col'>
             <SingleDetail
                 label='Password'
