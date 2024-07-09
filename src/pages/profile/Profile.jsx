74f7dee1f7d876986f0939b1f4a0f4d707a1abdc
ayopelumi <ayopelumi2014@gmail.com>
2024-07-09 03:17:59 -0700

    worked on the layout

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,46 +1,18 @@
-import { FaEdit } from "react-icons/fa";
+
 import BasicDetails from "./components/BasicDetails";
+import InstantDriverDetails from "./components/InstantDriverDetails";
 import LoginDetails from "./components/LoginDetails";
+
 const Profile = () => {
   return (
-    <section className='w-screen h-screen p-32'>
+    <section className='w-screen p-32'>
         <h3 className='text-[44px]/[56px] font-bold'>Profile Details</h3>    
 
-        <div className="w-fit ml-auto mr-auto">
+        <div className="w-3/4 ml-auto mr-auto">
             <BasicDetails />
             <LoginDetails />
-            <div className='flex mt-6 p-6 relative border-4 border-custom-grey'>
-                <h4 className=" bg-white text-lg font-medium absolute top-[-16px]">Instant Driver Details</h4>
-                <div className='flex flex-col'>
-                    <div>
-                        <span>
-                           Joined
-                        </span>
-                        <p>
-                            Ogundeji Oluwapelumi Ayodeji
-                        </p>
-                        <FaEdit />
-                    </div>
-                    <div>
-                        <span>
-                            Last Renewal
-                        </span>
-                        <p>
-                            Ogundeji Oluwapelumi Ayodeji
-                        </p>
-                        <FaEdit />
-                    </div>
-                    <div>
-                        <span>
-                            Next Renewal
-                        </span>
-                        <p>
-                            Ogundeji Oluwapelumi Ayodeji
-                        </p>
-                        <FaEdit />
-                    </div>
-                </div>
-            </div>
+            <InstantDriverDetails />
+
         </div>
     </section>
   )
