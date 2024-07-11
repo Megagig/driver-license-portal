fc8dc8380476c326f3bceed4016f3287da20e211
Megagig <megagigsoftwaresolutions.com>
2024-07-11 14:37:25 +0100

    Change the login and register icon

--- src/pages/auth/components/LoginHeader.jsx
+++ src/pages/auth/components/LoginHeader.jsx
@@ -12,7 +12,7 @@ export default function LoginHeader({
         <img
           alt=""
           className="h-14 w-14"
-          src="https://ik.imagekit.io/pibjyepn7p9/Lilac_Navy_Simple_Line_Business_Logo_CGktk8RHK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649962071315"
+          src="./src/assets/images/business.png"
         />
       </div>
       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
