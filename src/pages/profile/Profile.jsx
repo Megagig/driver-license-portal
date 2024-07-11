897365003d2d763b142cbf0174164c2cb3a02a12
IYKE <ikegwimik@gmail.com>
2024-07-11 15:20:53 +0100

    updated branch

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,23 +1,25 @@
 
+
 import BasicDetails from "./components/BasicDetails";
 import InstantDriverDetails from "./components/InstantDriverDetails";
 import LoginDetails from "./components/LoginDetails";
 import ProfilePicture from "./components/ProfilePicture";
 
 const Profile = () => {
-  return (
-    <section className='w-screen p-32'>
-        <h3 className='text-[44px]/[56px] font-bold'>Profile Details</h3>    
+    return (
+        <section className='w-screen p-32'>
+            <h3 className='text-[44px]/[56px] font-bold'>Profile Details</h3>
 
-        <div className="w-3/4">
-            <ProfilePicture />
-            <BasicDetails />
-            <LoginDetails />
-            <InstantDriverDetails />
+            <div className="w-3/4">
+                <ProfilePicture />
+                <BasicDetails />
+                <LoginDetails />
+                <InstantDriverDetails />
 
-        </div>
-    </section>
-  )
+
+            </div>
+        </section>
+    )
 }
 
 export default Profile
