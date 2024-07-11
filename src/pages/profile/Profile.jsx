4f7cf415182f0b15d497f180ce8bde887dffde5b
ayopelumi <ayopelumi2014@gmail.com>
2024-07-11 09:56:52 -0700

    worked on the profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import BasicDetails from "./components/BasicDetails";
 import InstantDriverDetails from "./components/InstantDriverDetails";
@@ -10,10 +10,28 @@ const Profile = () => {
 
   let { state } = useLocation();
   console.log('state:', state)
-  const [profileDetails, setProfile] = useState({})
-  useEffect(() => {
-    state ? setProfile(state) : null
-  }, [state]);
+  const [profileDetails, setProfile] = useState({
+      username: 'michaelw',
+        password: 'michaelwpass',
+        firstname: "Michael",
+        surname: "Joe",
+        middlename: "Doe",
+        dob:"1992-12-27",
+        phone:"+2348111206206",
+        email:"ayopelumi2014@gmail.com",
+        StateofAddress: "Lagos",
+        lga:"ifedayo",
+        address:"berger",
+        gender: "Male",
+        licenseId:"12344AD52DC",
+        certificateNumber:"Ikj384AD34",
+        nin :"557FFR2397F93983",
+        joined : "21-12-3034",
+        lastRenewal: "21-21-2021",
+        nextRenewal: "21-21-2021",
+        image: "https://github.com/Megagig/driver-license-portal/blob/13ac1e81aa634480f412b95d9c5a4e17c5007917/src/assets/images/close-up-friends-traveling-by-car.jpg"
+  })
+
 
   console.log({profileDetails})
 
