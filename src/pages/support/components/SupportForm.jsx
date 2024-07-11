3280ce9cfbccc58a48e3364a29a56bfb5f4e073d
IYKE <ikegwimik@gmail.com>
2024-07-11 14:51:24 +0100

    added modal to support and verify page

--- src/pages/support/components/SupportForm.jsx
+++ src/pages/support/components/SupportForm.jsx
@@ -1,6 +1,8 @@
 
 
-const SupportForm = () => {
+const SupportForm = ({ BtnFunction }) => {
+
+
     const inputStyle = "w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-4 text-base text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
 
     return (
@@ -9,36 +11,36 @@ const SupportForm = () => {
 
                 <div >
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Application ID <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="Enter Application ID" />
+                    <input className={inputStyle} type="text" placeholder="Enter Application ID" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Driver's Licence Number (dd/mm/yyyy) <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="e.g 01/01/2024" />
+                    <input className={inputStyle} type="text" placeholder="e.g 01/01/2024" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Date of Birth (dd/mm/yyyy) <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="e.g 01/01/2024" />
+                    <input className={inputStyle} type="text" placeholder="e.g 01/01/2024" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Full Name <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="e.g John Doe" />
+                    <input className={inputStyle} type="text" placeholder="e.g John Doe" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Contact Number <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="e.g +2349009000222" />
+                    <input className={inputStyle} type="text" placeholder="e.g +2349009000222" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Email Address <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="email" placeholder="e.g +2349009000222" />
+                    <input className={inputStyle} type="email" placeholder="e.g +2349009000222" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Comments <span className="text-red-500">*</span></label>
-                    <textarea className={inputStyle} name="" id=""></textarea>
+                    <textarea className={inputStyle} name="" id="" required></textarea>
                 </div>
 
             </div>
             <div className="mt-10 grid place-content-end">
-                <button className="bg-custom-green text-white py-2.5 px-8 rounded-md">Submit</button>
+                <button onClick={BtnFunction} className="bg-custom-green text-white py-2.5 px-8 rounded-md">Submit</button>
             </div>
         </div>
     )
--- src/pages/verification/Verify.jsx
+++ src/pages/verification/Verify.jsx
@@ -1,10 +1,55 @@
 import StepBox from "./components/StepBox"
 import handcard from "../../assets/hand-card.webp"
+import { useRef, useState } from "react";
 
+import VerificationFailed from "./components/VerificationFailed"
+import VerificationSuccess from "./components/VerificationSuccess"
 
 
 
 const Verify = () => {
+    window.scrollTo(0, 0)
+    const IDinput = useRef()
+
+    const [Popup, SetPopup] = useState(false)
+    const [Message, SetMessage] = useState()
+
+
+    let dummyId = "IDL1234567890"
+
+    const closePopup = () => {
+        SetPopup(false)
+        document.querySelector("body").classList.remove("h-screen")
+        document.querySelector("body").classList.remove("overflow-hidden")
+        console.log(Popup)
+    }
+
+    const verifyID = () => {
+        console.log(IDinput.current.value)
+        SetPopup(true)
+
+        if (!Popup) {
+            document.querySelector("body").classList.add("h-screen")
+            document.querySelector("body").classList.add("overflow-hidden")
+
+            if (IDinput.current.value === dummyId) {
+                SetMessage(
+                    <VerificationSuccess
+                        BtnFunction={closePopup}
+                    />
+                )
+            } else {
+                SetMessage(
+                    <VerificationFailed
+                        BtnFunction={closePopup}
+                    />
+                )
+            }
+        }
+
+    }
+
+
     return (
         <div className="p-4 md:px-20 h-full md:py-16 ">
             <div >
@@ -14,14 +59,15 @@ const Verify = () => {
                         <h1 className=" text-3xl md:text-5xl text-custom-green font-bold mb-8 md:mb-16">Verify License</h1>
 
                         <p className="text-justify">Securely verify your identity or someone else's with our driver's license verification service. This quick and easy process uses secure technology to protect your information. Get started today and streamline your verification needs.</p>
+                        <p>Sample ID: IDL1234567890 </p>
 
                         <div className="mt-10">
                             <label className="mb-[2px] block text-base font-medium text-neutral-700">License ID <span className="text-red-500">*</span></label>
-                            <input className="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-4 text-base text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" type="text" placeholder="Enter License ID to be verified" />
+                            <input ref={IDinput} className="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-4 text-base text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" type="text" placeholder="Enter License ID to be verified" />
                         </div>
 
                         <div className="mt-5 grid place-content-end">
-                            <button className="bg-custom-green text-white py-2.5 px-8 rounded-md">Submit</button>
+                            <button onClick={verifyID} className="bg-custom-green text-white py-2.5 px-8 rounded-md">Submit</button>
 
                         </div>
                     </div>
@@ -63,10 +109,15 @@ const Verify = () => {
 
                     </div>
                 </div>
+                {
+                    Popup && Message
+                }
 
 
 
             </div>
+
+
 
         </div>
     )
