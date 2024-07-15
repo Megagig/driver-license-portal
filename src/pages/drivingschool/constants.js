1f7543dcdc49bf32901f2b03de3e5dd63498547a
Megagig <megagigsoftwaresolutions.com>
2024-07-15 19:52:56 +0100

    Add the constants

---
+++ src/pages/drivingschool/constants.js
@@ -0,0 +1,40 @@
+import { BotMessageSquare } from 'lucide-react';
+import { BatteryCharging } from 'lucide-react';
+import { Fingerprint } from 'lucide-react';
+import { ShieldHalf } from 'lucide-react';
+import { PlugZap } from 'lucide-react';
+import { GlobeLock } from 'lucide-react';
+
+export const features = [
+  {
+    icon: <BotMessageSquare />,
+    school: 'Saftey driving school',
+    Address: 'Be good ventures Opp. Zariek Photos Maiduguri.',
+  },
+  {
+    icon: <Fingerprint />,
+    school: 'Trinitas Driving School',
+    Address: 'Basilica of the Most High Trinity Onitsha',
+  },
+  {
+    icon: <ShieldHalf />,
+    school: 'Journey international Driving School',
+    Address: 'Number 8 mission Road Awka, Anambra State.',
+  },
+  {
+    icon: <BatteryCharging />,
+    school: 'Unik Driving School',
+    Address: '14 Agodgba Avenue Parkview State, Lagos',
+  },
+  {
+    icon: <PlugZap />,
+    school: 'City Driving School',
+    Address: 'No 141 Zaria Road, Kano.',
+  },
+  {
+    icon: <GlobeLock />,
+    text: 'Analytics Dashboard',
+    description:
+      'Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.',
+  },
+];
