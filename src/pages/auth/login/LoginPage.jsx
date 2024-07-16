edbe426f3820bd3aaf5515b412d4f0aae3e23a05
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-07-16 11:58:12 +0100

    fix: Dynamically import images on auth pages

--- src/pages/auth/login/LoginPage.jsx
+++ src/pages/auth/login/LoginPage.jsx
@@ -1,12 +1,13 @@
 import Login from '../components/Login';
 import LoginHeader from '../components/LoginHeader';
+import loginImage from '../../../assets/license.png';
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col md:flex-row min-h-screen items-center gap-4 mx-8">
       <div
         className="md:w-1/2 h-auto md:h-screen bg-cover bg-center"
-        style={{ backgroundImage: "url('./src/assets/license.png')" }}
+        style={{ backgroundImage: `url(${loginImage})` }}
       ></div>
 
       <div className="flex flex-col justify-center md:w-1/2 p-4 ">
--- src/pages/auth/signup/SignupPage.jsx
+++ src/pages/auth/signup/SignupPage.jsx
@@ -1,12 +1,13 @@
 import LoginHeader from '../components/LoginHeader';
 import Signup from '../components/Signup';
+import signUpImage from '../../../assets/license.png';
 
 export default function SignupPage() {
   return (
     <div className="flex flex-col md:flex-row min-h-screen items-center gap-4 mx-8">
       <div
         className="md:w-1/2 h-auto md:h-screen bg-cover bg-center"
-        style={{ backgroundImage: "url('./src/assets/license.png')" }}
+        style={{ backgroundImage: `url(${signUpImage})` }}
       ></div>
 
       <div className="flex flex-col justify-center md:w-1/2 p-4">
