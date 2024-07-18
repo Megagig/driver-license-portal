3e529a41924aac8c6afee214374ee4fce92dc722
ayopelumi <ayopelumi2014@gmail.com>
2024-07-18 03:57:51 -0700

    worked on appointment search page

--- src/pages/appointment/AppointmentDashboard.jsx
+++ src/pages/appointment/AppointmentDashboard.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import Input from '../profile/utils/Input'
-
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 const data = 
     {
         username: 'michaelw',
@@ -21,6 +20,7 @@ const data =
         joined : "21-12-3034",
         lastRenewal: "21-21-2021",
         nextRenewal: "21-21-2021",
+        image: "https://github.com/Megagig/driver-license-portal/blob/13ac1e81aa634480f412b95d9c5a4e17c5007917/src/assets/images/close-up-friends-traveling-by-car.jpg",
         application:[
             {
                 applicationType: "new application",
@@ -72,17 +72,23 @@ const data =
                 applicationId :"QFREIU-12345-12GH9",
             }
         ],
-        image: "https://github.com/Megagig/driver-license-portal/blob/13ac1e81aa634480f412b95d9c5a4e17c5007917/src/assets/images/close-up-friends-traveling-by-car.jpg"
     }
 
 
  export const AppointmentDashboard = () => {
-    return (
-        <section className="w-screen grid">
+    const navigate = useNavigate();
 
-             <label  className=" mt-7 flex bg-transparent border border-[#E9E9E94D] rounded-full w-fit">
-                <Input  placeholder="Search by application Id" className='bg-transparent text-white px-[32px] py-[20px] border-0 outline-0'/>
-                <button className="bg-white text-[20px]/[19.2px] font-bold text-text-custom-black px-[20px] py-[8px]  rounded-full w-[190px]">Suscribe</button>
+    const fetchAppointmentSlip = () => { 
+        // fetch code here
+        console.log("i am running");
+      navigate('/appointment',  {state:{ ...data }} );
+     };
+    return (
+        <section className="w-screen grid place-content-center">
+
+             <label  className=" mt-7 flex bg-transparent border border-[#22C55E] rounded-full w-fit">
+                <input  placeholder="Search by application Id" className='bg-transparent text-black px-[32px] py-[20px] border-0 outline-0'/>
+                <button onClick={fetchAppointmentSlip} className="bg-[#22C55E] text-[20px]/[19.2px] font-bold text-white px-[20px] py-[8px]  rounded-full w-[190px]">Suscribe</button>
             </label>
             
             
