2822cd214ecd084a7250f07c93a748df249dffab
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-07-26 22:28:17 +0100

    feat: Consume the login endpoint

--- src/pages/auth/components/Login.jsx
+++ src/pages/auth/components/Login.jsx
@@ -1,131 +1,102 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { loginFields } from '../constants/FormFields';
-import FormAction from './FormAction';
-import FormExtra from './FormExtra';
-import Input from './Input';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { loginFields } from "../constants/FormFields";
+import FormExtra from "./FormExtra";
+import Input from "./Input";
 
-import { toast } from 'react-hot-toast';
-import { useNavigate } from 'react-router-dom';
-import useAuth from '../../../hooks/useAuth';
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import { login } from "../api";
 
 const fields = loginFields;
 let fieldsState = {};
-fields.forEach((field) => (fieldsState[field.id] = ''));
+fields.forEach((field) => (fieldsState[field.id] = ""));
 
 export default function Login({ paragraph, linkUrl, linkName }) {
-  const [loginState, setLoginState] = useState(fieldsState);
-  const { setAuth } = useAuth();
-  const navigate = useNavigate();
+    const [loginState, setLoginState] = useState(fieldsState);
+    const [error, setError] = useState("");
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const refreshToken = localStorage.getItem('refreshToken');
-    if (token && refreshToken) {
-      setAuth({ token, refreshToken });
-    }
-  }, [setAuth]);
+    const { setAuth } = useAuth();
+    const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setLoginState({ ...loginState, [e.target.id]: e.target.value });
-  };
+    const handleChange = (e) => {
+        setLoginState({ ...loginState, [e.target.id]: e.target.value });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    authenticateUser();
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-  const authenticateUser = async () => {
-    try {
-      const res = await axios.post(
-        'https://saviorte.pythonanywhere.com/api/login/',
-        {
-          email: loginState['email'],
-          password: loginState['password'],
-          expiresInMins: 30,
+        setError("");
+        setIsSubmitting(true);
+
+        const data = {
+            email: loginState["email"],
+            password: loginState["password"],
+        };
+        const loginResponse = await login(data);
+
+        if (loginResponse.access) {
+            setAuth(loginResponse);
+            sessionStorage.setItem("auth", JSON.stringify(loginResponse));
+            navigate("/dashboard");
+            return;
         }
-      );
-<<<<<<< HEAD
 
-      if (res.status === 200) {
-        const token = res.data.token;
-        const refreshToken = res.data.refresh;
+        setIsSubmitting(false);
+        setError("Invalid login details, please try again.");
+    };
 
-        // Store tokens in local storage
-        localStorage.setItem('token', token);
-        localStorage.setItem('refreshToken', refreshToken);
+    return (
+        <>
+            <form className="space-y-6 px-6 py-4" onSubmit={handleSubmit}>
+                {error && (
+                    <p className="bg-red-100 text-red-700 py-2 px-4 rounded-md">
+                        {error}
+                    </p>
+                )}
+                <div className="-space-y-px">
+                    {fields.map((field) => (
+                        <Input
+                            key={field.id}
+                            handleChange={handleChange}
+                            value={loginState[field.id]}
+                            labelText={field.labelText}
+                            labelFor={field.labelFor}
+                            id={field.id}
+                            name={field.name}
+                            type={field.type}
+                            isRequired={field.isRequired}
+                            placeholder={field.placeholder}
+                        />
+                    ))}
+                </div>
 
-        // Set auth state
-        setAuth({ token, refreshToken });
-
-        toast.success('Login successful!');
-        navigate('/dashboard');
-      }
-    } catch (error) {
-      console.error('Error during authentication:', error);
-      toast.error('Login failed. Please try again.');
-    }
-  };
-=======
->>>>>>> 38dd5fa (Signup and login Auth)
-
-      console.log(res.status);
-      // if (res.status === 200) {
-      //   const token = res.data.token;
-      //   const fullUserRes = await axios.get(
-      //     'https://saviorte.pythonanywhere.com/api/user/',
-      //     {
-      //       headers: {
-      //         Authorization: `Bearer ${token}`,
-      //       },
-      //     }
-      //   );
-
-      //   if (fullUserRes.status === 200) {
-      //     sessionStorage.setItem('user', JSON.stringify(fullUserRes.data));
-      //   }
-
-      //   const user = res.data;
-      //   setAuth({ user });
-      //   sessionStorage.setItem('auth', JSON.stringify({ user }));
-      //   navigate('/dashboard');
-      // }
-    } catch (error) {
-      console.error('Error during authentication:', error);
-    }
-  };
-  return (
-    <>
-      <form className="space-y-6 px-6 py-4" onSubmit={handleSubmit}>
-        <div className="-space-y-px">
-          {fields.map((field) => (
-            <Input
-              key={field.id}
-              handleChange={handleChange}
-              value={loginState[field.id]}
-              labelText={field.labelText}
-              labelFor={field.labelFor}
-              id={field.id}
-              name={field.name}
-              type={field.type}
-              isRequired={field.isRequired}
-              placeholder={field.placeholder}
-            />
-          ))}
-        </div>
-
-        <FormExtra />
-        <FormAction handleSubmit={handleSubmit} text="Login" />
-        <p className="mt-2 text-center text-sm text-gray-600">
-          {paragraph}{' '}
-          <Link
-            to={linkUrl}
-            className="font-medium text-custom-green hover:text-green-800"
-          >
-            {linkName}
-          </Link>
-        </p>
-      </form>
-    </>
-  );
+                <FormExtra />
+                <button
+                    className="bg-custom-green w-full hover:bg-green-600 px-4 py-2 text-white font-medium rounded-lg mt-4"
+                    onSubmit={handleSubmit}
+                    type="submit"
+                >
+                    {isSubmitting ? (
+                        <div className="flex justify-center gap-4">
+                            <div className="w-6 h-6 rounded-full animate-spin border-y-4 border-solid border-white border-t-transparent shadow-md"></div>
+                            <span>Logging in...</span>
+                        </div>
+                    ) : (
+                        "Login"
+                    )}
+                </button>
+                <p className="mt-2 text-center text-sm text-gray-600">
+                    {paragraph}{" "}
+                    <Link
+                        to={linkUrl}
+                        className="font-medium text-custom-green hover:text-green-800"
+                    >
+                        {linkName}
+                    </Link>
+                </p>
+            </form>
+        </>
+    );
 }
