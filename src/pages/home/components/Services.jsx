08083b1e56252b88267cc0a507113520a14c4133
ayopelumi <ayopelumi2014@gmail.com>
2024-07-25 17:14:19 -0700

    refactoring the UI & fetch function

--- src/pages/home/components/Services.jsx
+++ src/pages/home/components/Services.jsx
@@ -43,72 +43,10 @@ const Services = () => {
             <div className='xl:px-20 grid md:grid-cols-2 lg:grid-cols-3 mt-12 lg:mt-20 gap-10 lg:gap-8'>
 
                 {arrayService.map((item, index) => {
-                    <Card image={item.image} title={item.title} key={`service-${index}`} />
+                   return <Card image={item.image} title={item.title} key={`service-${index}`} />
                     
                 })}
-
-                <div className='shadow-md  border-[#F4F4F4] border-t-[#ffc169] border-t-[16px] border bg-slate-100 flex flex-col rounded-xl p-4 md:p-10 gap-4 md:gap-7 items-start '>
-                  <div style={{backgroundImage :`url(${certificate})`}} className={`w-10 h-10 md:w-16 md:h-16 rounded-lg bg-contain bg-[url("${certificate}")] self-center`}></div>
-                    <p className='font-semibold text-base md:text-[23px]/[26.4px] self-center'>
-                        New Application
-                    </p>
-                    <p className='font-normal text-sm md:text-[17px]/[22px] text-wrap flex-1 text-left'>
-                    ✓  Applicant must be 18 years and above <br/>
-                        ✓   Apllicant must have completed driving school
-                    </p>
-                </div>
-                <div className='shadow-md  border-[#F4F4F4] border-t-[#83c6f3] border-t-[16px] border bg-[#d5ffe4] flex flex-col rounded-xl p-4 md:p-10 gap-4 md:gap-7 items-start'>
-                  <div style={{backgroundImage :`url(${renewal})`}} className={`w-10 h-10 md:w-16 md:h-16 rounded-lg bg-contain self-center`}></div>
-                    <p className='font-semibold text-base md:text-[23px]/[26.4px] self-center'>
-                       Renew
-                   </p>
-                   <p className='font-normal text-black text-sm md:text-[17px]/[22px] text-wrap flex-1 text-left'>
-                    ✓  Applicant must be 18 years and above<br/>
-                    ✓  Apllicant must have completed driving school
-                    </p>
-                </div>
-                <div className='shadow-md  border-[#F4F4F4] border-t-[#4cffff] border-t-[16px] border bg-slate-100 md:bg-[#d5ffe4] lg:bg-slate-100 flex flex-col rounded-xl p-4 md:p-10 gap-4 md:gap-7 items-start'>
-                  <div style={{backgroundImage :`url(${business})`}} className={`w-10 h-10 md:w-16 md:h-16 rounded-lg bg-contain self-center`}></div>
-                    <p className='font-semibold text-base md:text-[23px]/[26.4px] self-center'>
-                            Reissue
-                        </p>
-                        <p className='font-normal text-black text-sm md:text-[17px]/[22px] text-wrap flex-1 text-left'>
-                        ✓   Applicant must be 18 years and above <br/>
-                            ✓   Apllicant must have completed driving school                     
-                        </p>
-                </div>
-                <div className='shadow-md  border-[#F4F4F4] border-t-[#ffc169] border-t-[16px] border bg-[#d5ffe4] md:bg-slate-100 lg:bg-[#d5ffe4] flex flex-col rounded-xl p-4 md:p-10 gap-4 md:gap-7 items-start'>
-                  <div style={{backgroundImage :`url(${edit})`}} className={`w-10 h-10 md:w-16 md:h-16 rounded-lg bg-contain self-center`}></div>
-                    <p className='font-semibold text-base md:text-[23px]/[26.4px] self-center'>
-                        Edit Application
-                    </p>
-                    <p className='font-normal text-black text-sm md:text-[17px]/[22px] text-wrap flex-1 text-left'>
-                    ✓  Applicant must be 18 years and above <br/>
-                    ✓     Apllicant must have completed driving school
-                    </p>                   
-                </div>
-                <div className='shadow-md  border-[#F4F4F4] border-t-[#83c6f3] border-t-[16px] border bg-slate-100 flex flex-col rounded-xl p-4 md:p-10 gap-4 md:gap-7 items-start'>
-                  <div style={{backgroundImage :`url(${print})`}} className={`w-10 h-10 md:w-16 md:h-16 rounded-lg bg-contain self-center`}></div>
-                    <p className='font-semibold text-base md:text-[23px]/[26.4px] self-center'>
-                        License Verification
-                    </p>
-                    <p className='font-normal text-black text-sm md:text-[17px]/[22px] text-wrap flex-1 text-left'>
-                    
-                    ✓  Applicant must be 18 years and above<br/>
-                    ✓  Apllicant must have completed driving school
-                    </p>
-                </div>
-                <div className='group shadow-xl hover:bg-green-400 hover:shadow-xl  hover:scale-110 hover:text-white transform-style-3d text-black  border-[#F4F4F4] border-t-[#4cffff] border-t-[16px] border bg-[#d5ffe4] flex flex-col rounded-xl p-4 md:p-10 gap-4 md:gap-7 items-start '>
-                  <div style={{backgroundImage :`url(${print})`}} className={`group-hover:invert w-10 h-10 md:w-16 md:h-16 rounded-lg bg-contain self-center`}></div>
-                    <p className='group-hover:translate-z-28   font-semibold text-base md:text-[23px]/[26.4px] text-wrap self-center'>
-                        Print Acknowledgement Slip
-                    </p>
-                    <p className='font-normal text-sm md:text-[17px]/[22px] text-wrap flex-1 text-left'>
-                    ✓   Applicant must be 18 years and above <br/>
-                    ✓    Apllicant must have completed driving school                     
-                    </p>
-                </div>
-                <Card />      
+   
             </div>
 
 
--- src/pages/home/utils/Card.jsx
+++ src/pages/home/utils/Card.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 const Card = (props) => {
     return (
-        <div className="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-            <div
-                className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-                <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+        <div className="relative flex flex-col justify-center overflow-hidden bg-gray-50 py-4 md:py-8">
+            <div className="h-[400px] group relative cursor-pointer overflow-hidden bg-white px-6 pt-7 pb-5 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:scale-110 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg md:px-10">
+                <span className="absolute top-10 z-0 h-20 w-20 rounded-full bg-green-400 transition-all duration-300 group-hover:scale-[10]"></span>
                 <div className="relative z-10 mx-auto max-w-md">
-                    <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
-                        <img src={props.img} alt="image"/>
+                    <span className=" flex h-52 w-52 group-hover:h-28 group-hover:w-28 items-center justify-center rounded-full bg-green-400 transition-all duration-300 group-hover:bg-green-300">
+                        <img src={props.image} alt="image" className="w-3/4 group-hover:w-full object-fit invert p-6"/>
                     </span>
-                    <p className='group-hover:translate-z-28   font-semibold text-base md:text-[23px]/[26.4px] text-wrap self-center'>
+                    <p className='mt-24 mb-24 group-hover:mb-0 group-hover:mt-4 group-hover:translate-z-28   font-semibold text-base md:text-[23px]/[26.4px] text-wrap self-center transition-all duration-300 group-hover:text-white/90'>
                         {props.title}
                     </p>
                     <div className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
