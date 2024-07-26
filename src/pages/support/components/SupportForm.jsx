82773a015f6c866c2fb866d1391599d65a9acd58
IYKE <ikegwimik@gmail.com>
2024-07-26 16:19:41 +0100

    created svg version for logo

--- src/pages/support/components/SupportForm.jsx
+++ src/pages/support/components/SupportForm.jsx
@@ -1,16 +1,48 @@
 import Button from "../../../components/utils/Button"
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
+
+
+
 
 const SupportForm = ({ BtnFunction }) => {
-    const [phoneNo, setPhoneNo] = useState("")
-    const [formData, setFormData] = useState({})
+    const [value, setValue] = useState(null)
 
+
+    const [formData, setFormData] = useState({
+
+    })
+
+
+
+    useEffect(() => {
+        setFormData({
+            ...formData,
+            phoneNo: value,
+        })
+
+    }, [value])
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        })
+
+    }
+
+
+    console.log(formData)
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log("you submitted a form")
     }
+
+
+
+
 
     const inputStyle = "w-full rounded-md border border-[#e0e0e0] py-2 px-4 text-base text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
 
@@ -20,19 +52,19 @@ const SupportForm = ({ BtnFunction }) => {
 
                 <div >
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Application ID <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="Enter Application ID" required />
+                    <input name="applicationId" onChange={handleChange} className={inputStyle} type="text" placeholder="Enter Application ID" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Driver's Licence Number <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="e.g 01/01/2024" required />
+                    <input name="licenseNo" onChange={handleChange} className={inputStyle} type="text" placeholder="e.g 01/01/2024" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Date of Birth (dd/mm/yyyy) <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="date" placeholder="e.g 01/01/2024" required />
+                    <input name="dob" onChange={handleChange} className={inputStyle} type="date" placeholder="e.g 01/01/2024" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Full Name <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="text" placeholder="e.g John Doe" required />
+                    <input name="fullName" onChange={handleChange} className={inputStyle} type="text" placeholder="e.g John Doe" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Contact Number <span className="text-red-500">*</span></label>
@@ -43,20 +75,19 @@ const SupportForm = ({ BtnFunction }) => {
                             placeholder="e.g +2349009000222"
                             countryCallingCodeEditable={false}
                             defaultCountry="NG"
-                            value={phoneNo}
-                            onChange={setPhoneNo}
-
+                            value={value}
+                            onChange={setValue}
                         />
 
                     </div>
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Email Address <span className="text-red-500">*</span></label>
-                    <input className={inputStyle} type="email" placeholder="e.g +2349009000222" required />
+                    <input name="email" onChange={handleChange} className={inputStyle} type="email" placeholder="e.g +2349009000222" required />
                 </div>
                 <div>
                     <label className="mb-[2px] block text-base font-medium text-neutral-700">Comments <span className="text-red-500">*</span></label>
-                    <textarea className={inputStyle} name="" id="" required></textarea>
+                    <textarea name="comments" onChange={handleChange} className={inputStyle} id="" required></textarea>
                 </div>
 
             </form>
