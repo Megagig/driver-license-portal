ec4b1a63236838e2300dec08aad372c02921f37b
ayopelumi <ayopelumi2014@gmail.com>
2024-07-28 13:46:52 -0700

    worked on homeage UI

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,7 +3,6 @@ import CounterDisplay from './components/CounterDisplay'
 import { Hero } from "./components/Hero"
 import LicenseProcess from './components/LicenseProcess'
 import Services from './components/Services'
-import Testimonials from './components/Testimonials'
 
 const HomePage = () => {
     return (
@@ -12,7 +11,7 @@ const HomePage = () => {
             <LicenseProcess />
             <Services />
             <CounterDisplay />
-            <Testimonials />
+            {/* <Testimonials /> */}
         </div>
     )
 }
