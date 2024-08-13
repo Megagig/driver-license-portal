36788bf3d8baa31a85426297165a1c4f1447dab1
Megagig <megagigsoftwaresolutions.com>
2024-08-13 01:45:23 +0100

    Add password rules for validation

--- src/pages/authentication/signup/SignUp.jsx
+++ src/pages/authentication/signup/SignUp.jsx
@@ -1,3 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { createAccount } from '../../auth/api';
+import WelcomeModal from '../components/WelcomeModal';
+
 const SignUp = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -9,6 +15,17 @@ const SignUp = () => {
   const [errors, setErrors] = useState({});
   const [showPasswordRules, setShowPasswordRules] = useState(false);
   const [showWelcomeModal, setShowWelcomeModal] = useState(false);
+
+  const passwordRules = [
+    { rule: /.{8,24}/, text: '8-24 characters' },
+    { rule: /[A-Z]/, text: 'At least 1 capital letter' },
+    { rule: /\d/, text: 'At least 1 digit' },
+    { rule: /[!#@$_\-*&%]/, text: 'At least 1 special character' },
+    {
+      rule: /^[A-Za-z\d!#@$_\-*&%]{8,24}$/,
+      text: 'Allowed special characters: ! # @ $ _ - * & %',
+    },
+  ];
   return (
     <div>
       <h1>Sign in</h1>
