f4bc7f8e3492022c824f3f07037b01aa3b1df8d2
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-08-14 10:08:45 +0100

    style: Add general spacing to header & main

--- src/components/layouts/admin/AdminLayout.jsx
+++ src/components/layouts/admin/AdminLayout.jsx
@@ -9,15 +9,15 @@ const AdminLayout = () => {
 
     return (
         <div className="flex w-screen min-h-screen">
-            <aside className="w-64 hidden md:inline">Side Nav</aside>
+            <aside className="w-64 hidden md:inline border-r border-[#E6EFF5]">Side Nav</aside>
 
             <div className="flex-1 flex flex-col">
-                <header className="py-8 px-10">Navbar</header>
-                <main className="bg-[#F5F7FA] flex-1">
+                <header className="py-8 px-10 border-l border-b border-[#E6EFF5]">Navbar</header>
+                <main className="bg-[#F5F7FA] flex-1 px-10 py-8">
                     <Outlet />
-                    {/* <ReusableModal isOpen={isLoading}>
+                    <ReusableModal isOpen={isLoading}>
                         <Spinner />
-                    </ReusableModal> */}
+                    </ReusableModal>
                 </main>
             </div>
         </div>
