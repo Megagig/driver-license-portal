efe623d7ae7fbb52fa1483ba19bb7028d3cd5338
IYKE <ikegwimik@gmail.com>
2024-08-15 10:27:01 +0100

    completed desktop side and nav bar

--- src/pages/admin/dashboard/AdminDashboard.jsx
+++ src/pages/admin/dashboard/AdminDashboard.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useOutletContext } from 'react-router-dom'
 
 const AdminDashboard = () => {
+
+  const { setPageName } = useOutletContext()
+
+  useEffect(() => {
+    setPageName('Overview')
+  }, [])
+
   return (
-    <div>AdminDashboard</div>
+    <>
+      <h1 className='md:hidden font-semibold text-2xl pb-4'>OverView</h1>
+      <div>AdminDashboard</div>
+    </>
+
   )
 }
 
--- src/pages/admin/dlc/Dlc.jsx
+++ src/pages/admin/dlc/Dlc.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useEffect } from "react"
+import { useOutletContext } from "react-router-dom"
+
+
+
 
 const Dlc = () => {
+
+  const { setPageName } = useOutletContext()
+
+  useEffect(() => {
+    setPageName('DLC Records')
+  }, [])
+
   return (
     <div>Dlc</div>
   )
