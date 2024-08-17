501e4337dbb7f4ec1b8a0992f37b5c97d9adc7ce
IYKE <ikegwimik@gmail.com>
2024-08-17 19:55:02 +0100

    resolved thin footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,13 @@
 import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { IoLogoYoutube } from "react-icons/io";
 import { Link } from "react-router-dom";
-import logo from "../assets/Logo.svg";
+import logo from "../assets/Logo1.svg";
 import coatofarm from "../assets/coatOfArm.png";
 
 
 const Footer = () => {
 
-    const linkStyle = "flex font-thin hover:translate-x-1  transition-all hover:text-[#15803D] duration-100 text-[#093A51] gap-2"
-    const linkheadStyle = "font-thin text-lg mb-4 text-[#22D665]"
+    const linkStyle = "flex hover:translate-x-1  transition-all hover:text-[#15803D] duration-100 text-[#093A51] gap-2"
+    const linkheadStyle = "text-lg mb-4 text-[#22D665]"
     const iconStyle = " hover:text-green-600 hover:-translate-y-2 transition-all duration-200 text-3 cursor-pointer grid place-content-center h-10 w-10 rounded-full p-1 border border-[#1ACC00]"
     return (
         <footer className="bg-[#F3FFF8]  ">
@@ -130,9 +129,9 @@ const Footer = () => {
 
             </div>
             <div className="flex  flex-col lg:flex-row gap-10   px-4 md:px-10 xl:px-20 justify-between pb-4 border-b border-b-[#15803D] lg:items-center mt-[60px]">
-                <div className="md:flex gap-6 hidden ">
-                    <img className="h-12 md:h-14" src={coatofarm} alt="" />
-                    <img className="h-10 md:h-12" src={logo} alt="" />
+                <div className="md:flex items-center gap-6 hidden ">
+                    <img className="h-10 md:h-14" src={coatofarm} alt="" />
+                    <img className="h-10 md:h-10" src={logo} alt="" />
                 </div>
                 <ul className="flex flex-col md:flex-row gap-4">
                     <li>Privacy Policy</li>
