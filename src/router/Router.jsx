c5387b05e3fc517d671b9757fd150c2aff4f8814
IYKE <ikegwimik@gmail.com>
2024-08-18 15:28:29 +0100

    fixed rebase error

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -10,10 +10,9 @@ import ApplicationForm, { applicationFormLoader } from "../pages/applications/Ap
 import ApplicationHome from "../pages/applications/ApplicationHome";
 import Appointment, {
   loader as appointmentLoader,
+
 } from "../pages/appointment/Appointment";
 import {
-  AppointmentDashboard,
-  appointmentDashboardLoader,
   AppointmentDashboard,
   appointmentDashboardLoader,
 } from "../pages/appointment/AppointmentDashboard";
@@ -41,24 +40,13 @@ import {
   Settings,
   Login,
   SignUp,
-  AdminDashboard,
-  Dlc,
-  Dssp,
-  Frsc,
-  Vio,
-  NewApplicants,
-  Payments,
-  Reissue,
-  Renewal,
-  Settings,
-  Login,
-  SignUp,
 } from "../pages/admin";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className="min-h-screen w-screen overflow-x-hidden relative"><Outlet /></div>,
+
+    element: <div className="min-h-screen w-[dwv] overflow-x-hidden relative"><Outlet /></div>,
     children: [
       {
         path: "/",
@@ -200,154 +188,9 @@ const router = createBrowserRouter([
       },
     ]
   }
-  {
-    path: "/",
-    element: <div className="min-h-screen w-[dwv] overflow-x-hidden relative"><Outlet /></div>,
-    children: [
-      {
-        path: '/',
-        element: <Root />,
-        children: [
-          {
-            index: true,
-            element: <HomePage />,
-          },
-          {
-            path: "faq",
-            element: <Faq />,
-          },
-          {
-            path: "support",
-            element: <Support />,
-          },
-          {
-            path: "login",
-            element: <LoginPage />,
-          },
-          {
-            path: "signup",
-            element: <SignupPage />,
-          },
-          {
-            path: "verify-license",
-            element: <Verify />,
-          },
-          {
-            path: "capture-centers",
-            element: <CaptureCenter />,
-          },
-          {
-            path: "driving-schools",
-            element: <DrivingSchool />,
-          },
-          {
-            path: "*",
-            element: <PageNotFound />,
-          },
-          {
-            element: <AuthLayout />,
-            children: [
-              {
-                path: "dashboard",
-                element: <Dashboard />,
-              },
-              {
-                path: "profile",
-                element: <Profile />,
-                loader: profileLoader,
-              },
-              {
-                path: "applications/:type",
-                element: <ApplicationHome />,
-              },
-              {
-                path: "applications/:type/form",
-                element: <ApplicationForm />,
-                loader: applicationFormLoader
-              },
-              {
-                path: "get-appointment-slip",
-                element: <AppointmentDashboard />,
-                loader: appointmentDashboardLoader,
-              },
-              {
-                path: "appointment",
-                element: <Appointment />,
-                loader: appointmentLoader,
-              },
-              {
-                path: "schedule-appointment",
-                element: <div>Schedule Appointment Component</div>,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "admin",
-        element: <AdminLayout />,
-        children: [
-          {
-            index: true,
-            element: <Login />,
-          },
-          {
-            path: "signup",
-            element: <SignUp />,
-          },
-          {
-            element: <AdminAuthLayout />,
-            children: [
-              {
-                path: "dashboard",
-                element: <AdminDashboard />,
-              },
-              {
-                path: "payments",
-                element: <Payments />,
-              },
-              {
-                path: "new-applicants",
-                element: <NewApplicants />,
-              },
-              {
-                path: "renewal",
-                element: <Renewal />,
-              },
-              {
-                path: "reissue",
-                element: <Reissue />,
-              },
-              {
-                path: "dssp",
-                element: <Dssp />,
-              },
-              {
-                path: "vio",
-                element: <Vio />,
-              },
-              {
-                path: "dlc",
-                element: <Dlc />,
-              },
-              {
-                path: "frsc",
-                element: <Frsc />,
-              },
-              {
-                path: "settings",
-                element: <Settings />,
-              },
-            ],
-          },
-        ],
-      },
-    ]
-  }
 ]);
 
 const Router = () => {
-  return <RouterProvider router={router} />;
   return <RouterProvider router={router} />;
 };
 
